161aa4d39c984b63d0ec1000888de1f146a09e77
Eric Cheung <cheung.ec@gmail.com>
2013-08-17 05:21:32 +0000

    Edited content

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+//var app = express(express.logger());
 
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
@@ -11,7 +12,7 @@ app.get('/', function(request, response) {
     response.send(myBuffer.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
